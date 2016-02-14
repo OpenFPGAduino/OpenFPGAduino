53a5dbee26f66ce38a36035529d60854eaa293d6
lizhizhou <lzz@meteroi.com>
2016-02-14 16:06:28 +0800

    update docker version to 2.0

--- start-build-env.sh
+++ start-build-env.sh
@@ -1,5 +1,5 @@
-docker pull openfpgaduino/openfpgaduino:v1.0
+docker pull openfpgaduino/openfpgaduino:v2.0
 docker run --rm=true --env-file=docker_env --privileged -i -t \
   -v "${PWD}:/home/" \
   -w "/home/" \
-  openfpgaduino/openfpgaduino:v1.0
+  openfpgaduino/openfpgaduino:v2.0
