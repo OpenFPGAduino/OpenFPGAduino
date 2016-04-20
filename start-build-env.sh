f559b0e2f55a299ec0365c115de93061d8c5f98d
lizhizhou <lzz@meteroi.com>
2016-04-20 13:42:04 +0800

    add dns

--- start-build-env.sh
+++ start-build-env.sh
@@ -2,4 +2,5 @@ docker pull openfpgaduino/openfpgaduino:v2.0
 docker run --rm=true --env-file=docker_env --privileged -i -t \
   -v "${PWD}:/home/" \
   -w "/home/" \
+  --dns=114.114.114.114 \
   openfpgaduino/openfpgaduino:v2.0
